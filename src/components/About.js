1184715eb598550e8d88457be4808823bdf35858
shylajahande <shylajahande@gmail.com>
2019-11-22 08:39:27 +0530

    Added Text on About

--- src/components/About.js
+++ src/components/About.js
@@ -5,6 +5,7 @@ const About = ()=>{
 		<div className="container">
 			<h1 className="pageTitle">About</h1>
 			<p>I am Shylaja , Edited the About page</p>
+		<p>Site Deployed to Netlify.com</p>
 		</div>
 	)
 }
